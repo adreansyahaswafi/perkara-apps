a69c9750be1987d0298db28dc553bdcaaa6a1c08
adreansyah <adreansyah.aswafi@sg-edts.com>
2025-08-11 16:40:50 +0700

    feat():push

--- src/pages/police_report/detail.jsx
+++ src/pages/police_report/detail.jsx
@@ -95,7 +95,7 @@ const Detail = ({ title, level }) => {
                 </div>
 
                 <div className="flex my-2 gap-2">
-                    <div className="w-52 font-semibold">Terlapor</div>
+                    <div className="w-52 font-semibold">Saksi</div>
                     <div>:</div>
                     <div className="flex-1 whitespace-pre-wrap">{data?.data?.content?.saksi}</div>
                 </div>
